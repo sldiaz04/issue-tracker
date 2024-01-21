2cf2f2ee420a6f8c220adef13e0865cba9cdc428
Sergio Lopez Diaz <sldiaz04us@gmail.com>
2024-01-21 16:35:45 -0600

    Create the ErrorMessage.tsx component

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+import {PropsWithChildren} from "react";
+import {Text} from "@radix-ui/themes";
+
+const ErrorMessage = ({children}: PropsWithChildren) => {
+    if (!children) return;
+
+    return (
+        <Text color='red' as='p'>{children}</Text>
+    );
+}
+
+export default ErrorMessage;
