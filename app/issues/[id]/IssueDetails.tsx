011efd5cf12b0bde28a8b3df18a1669e4e3095dd
Sergio Lopez Diaz <sldiaz04us@gmail.com>
2024-01-23 21:15:59 -0600

    Refactor: Apply the SRP

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,21 @@
+import {Card, Flex, Heading, Text} from "@radix-ui/themes";
+import {IssueStatusBadge} from "@/app/components";
+import ReactMarkdown from "react-markdown";
+import {Issue} from "@prisma/client";
+
+const IssueDetails = ({issue}: { issue: Issue }) => {
+    return (
+        <>
+            <Heading as='h2'>{issue.title}</Heading>
+            <Flex gap='2' my='2'>
+                <IssueStatusBadge status={issue.status}/>
+                <Text>{issue.createAt.toDateString()}</Text>
+            </Flex>
+            <Card className='prose' mt='4'>
+                <ReactMarkdown>{issue.description}</ReactMarkdown>
+            </Card>
+        </>
+    );
+}
+
+export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,11 +1,8 @@
 import prisma from '@/prisma/client'
 import {notFound} from "next/navigation";
-import {Box, Button, Card, Flex, Grid, Heading, Text} from "@radix-ui/themes";
-import { Pencil2Icon} from '@radix-ui/react-icons'
-
-import {IssueStatusBadge} from "@/app/components";
-import ReactMarkdown from "react-markdown";
-import Link from "next/link";
+import {Box, Grid} from "@radix-ui/themes";
+import EditIssueButton from "@/app/issues/[id]/EditIssueButton";
+import IssueDetails from "@/app/issues/[id]/IssueDetails";
 
 interface Props {
     params: { id: string };
@@ -23,20 +20,10 @@ const IssueDetailPage = async ({params}: Props) => {
     return (
         <Grid columns={{initial: "1", md: "2"}} gap="5">
             <Box>
-                <Heading as='h2'>{issue.title}</Heading>
-                <Flex gap='2' my='2'>
-                    <IssueStatusBadge status={issue.status}/>
-                    <Text>{issue.createAt.toDateString()}</Text>
-                </Flex>
-                <Card className='prose' mt='4'>
-                    <ReactMarkdown>{issue.description}</ReactMarkdown>
-                </Card>
+                <IssueDetails issue={issue}/>
             </Box>
             <Box>
-                <Button>
-                    <Pencil2Icon/>
-                    <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-                </Button>
+                <EditIssueButton issueId={issue.id}/>
             </Box>
         </Grid>
     );
