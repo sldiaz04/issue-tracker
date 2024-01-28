41035dd00e988497eff55a83f99aeb688285df05
Sergio Lopez Diaz <sldiaz04us@gmail.com>
2024-01-28 17:50:43 -0600

    Lay out the Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 import IssueSummary from "@/app/IssueSummary";
 import IssueChart from "@/app/IssueChart";
+import {Flex, Grid} from "@radix-ui/themes";
+import LatestIssue from "@/app/LatestIssue";
 
 export default async function Home() {
     const totalOpen = await prisma?.issue.count({where: {status: 'OPEN'}}) || 0;
-    const totalInProgress = await prisma?.issue.count({where: {status: 'IN_PROGRESS'}}) || 0;;
-    const totalClosed = await prisma?.issue.count({where: {status: 'CLOSED'}}) || 0;;
+    const totalInProgress = await prisma?.issue.count({where: {status: 'IN_PROGRESS'}}) || 0;
+    const totalClosed = await prisma?.issue.count({where: {status: 'CLOSED'}}) || 0;
 
     return (
-        // <LatestIssue/>
-        // <IssueSummary open={totalOpen} inProgress={totalInProgress} closed={totalClosed}/>
-        <IssueChart open={totalOpen} inProgress={totalInProgress} closed={totalClosed}/>
+        <Grid columns={{initial: "1", md: "2"}} gap="5">
+            <Flex direction='column' gap="5">
+                <IssueSummary open={totalOpen} inProgress={totalInProgress} closed={totalClosed}/>
+                <IssueChart open={totalOpen} inProgress={totalInProgress} closed={totalClosed}/>
+            </Flex>
+            <LatestIssue/>
+        </Grid>
     )
 }
